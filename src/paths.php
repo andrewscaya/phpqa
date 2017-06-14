66fbe2ad6fcdb3b8e286db397606814fe9cdab85
Elizabeth M Smith <auroraeosrose@gmail.com>
2017-06-14 10:46:38 -0400

    fix spaces and special characters in binary path
    Makes a binary path quoted so it doesn't bork if there are spaces or weird characters in paths

--- src/paths.php
+++ src/paths.php
@@ -4,7 +4,8 @@ namespace Edge\QA;
 
 function pathToBinary($tool)
 {
-    return COMPOSER_BINARY_DIR . $tool;
+    // slightly hacky fix that allows "binary" paths with spaces to run properly
+    return escapePath(realpath(COMPOSER_BINARY_DIR . $tool));
 }
 
 function escapePath($path)
