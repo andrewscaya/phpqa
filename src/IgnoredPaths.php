17f7cd9f44027221df31163dc68d84b657dab1bc
Elizabeth M Smith <auroraeosrose@gmail.com>
2017-06-14 15:52:12 -0400

    phpmd and pdepend
    Yeah, so not normalizing slash separators plus using regex which means slashes matter mean this is broken as all getout on windows
    
    Make the ignores cooperate... hopefullyt

--- src/IgnoredPaths.php
+++ src/IgnoredPaths.php
@@ -25,14 +25,39 @@ class IgnoredPaths
         return $this->ignore(' --ignore=*/', '/*,*/', '/*', ',');
     }
 
-    public function pdepend()
+    // pdepend and phpmd use the same filter for their files
+    // https://github.com/pdepend/pdepend/blob/master/src/main/php/PDepend/Input/ExcludePathFilter.php
+    // rule1: you need /* after any directory to get all the insides, how annoying
+    // rule2: SLASHES MATTER - so on windows you CANNOT use forward slashes!
+    // this is a quick and dirty fix, I could probably buckle down and make it nice
+    // and I don't care about files so no files are done here, I know, bad me, WORK WORK WORK DANGIT
+    public function pdepend($list_only = false)
     {
-        return $this->ignore(' --ignore=', ',', '', ',');
+        // first we force slashes
+        $ignores = $this->ignoreDirs;
+        foreach($ignores as $key => $path) {
+            $ignores[$key] = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+        }
+        
+        // then we pop \* versions for all the things
+        foreach($ignores as $path) {
+            $ignores[] = $path . DIRECTORY_SEPARATOR . '*';
+        }
+        
+        if($list_only) {
+            return $ignores;
+        }
+        
+        return $this->implode($ignores, ' --ignore', ',', '');
     }
 
     public function phpmd()
     {
-        return $this->ignore(" --exclude /", '/,/', '/', ',/');
+        //build our list identically to what we did for pdepends madness
+        $list = $this->pdepend(true);
+        
+        // return with --exclude instead of --ignore - SILLY THINGS
+        return $this->implode($ignores, ' --exclude', ',', '');
     }
 
     public function phpmetrics()
